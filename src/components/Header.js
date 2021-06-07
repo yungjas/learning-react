94737fca59af28d3b2537b86901202c2967ea8ab
jas <jasmine-lim88@hotmail.com>
2021-06-07 14:36:16 +0800

    proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 //props is used to pass in stuff to our components
 //pass in title object to Header component ----> const Header = ({title})
 const Header = ({title}) => {
@@ -10,6 +12,12 @@ const Header = ({title}) => {
 
 Header.defaultProps = {
     title: "Task Tracer",
+}
+
+//check the type of the prop, e.g. the title prop should be string and is required
+//helps catch type related errors
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
