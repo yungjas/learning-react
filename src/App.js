6a84d2350ccc216a7036a6a225effd13b2eb4a69
jas <jasmine-lim88@hotmail.com>
2021-07-13 17:08:22 +0800

    migrate data to json file

--- src/App.js
+++ src/App.js
@@ -13,26 +13,7 @@ function App() {
     //setTasks is a function used to update the state i.e. add more tasks
     //state is immutable, you can't change it directly --> can't use tasks.push to update the state
     //placing tasks here so that it becomes a global state i.e. every component can access it 
-    const [tasks, setTasks] = useState ([
-      {
-          id: 1,
-          text: "Doctors Appointment",
-          day: "Feb 5th at 2.30pm",
-          reminder: true
-      },
-      {
-          id: 2,
-          text: "Meeting at School",
-          day: "Feb 6th at 2.30pm",
-          reminder: true
-      },
-      {
-          id: 3,
-          text: "Food Shopping",
-          day: "Feb 7th at 2.30pm",
-          reminder: false
-      },
-  ])
+    const [tasks, setTasks] = useState ([])
   
   //Add task
   function addTask(task){
